a02e649c605f3a076f0e5227e5d0b66fe6454296
JuankoGmzk <37718099+JuankoGmzk@users.noreply.github.com>
2021-06-11 16:34:27 -0500

    Component Grades - Correción de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginModel} from '../models/login.model';
+import {LoginService} from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  user : LoginModel;
+  constructor(private loginService : LoginService) {
+    this.user = new LoginModel();
+   }
+  
   ngOnInit(): void {
   }
 
+  LoginUser(): void {
+    console.log(this.user.email, this.user.password)
+    this.loginService.postLogin(this.user);
+  }
 }
