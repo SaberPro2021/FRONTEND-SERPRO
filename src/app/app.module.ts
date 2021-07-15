876b5b4583f69d503d61e5a5d8abbf3306024c3d
Juan Pablo <juanpis0304@hotmail.com>
2021-07-14 19:19:56 -0500

    Cambios pequeños en el front con el CORS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // PROJECT COMPONENTS
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {  RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { IcfesModulesListComponent } from './serpro-components/icfes-modules-list/icfes-modules-list.component';
 import { TestListComponent } from './serpro-components/modules-test/test-list.component';
 import { IcfesTestComponent } from './serpro-components/modules-test/icfes-test/icfes-test.component';
 import { MultipleSelectionQuestionComponent } from './serpro-components/modules-test/questions/multiple-selection-question/multiple-selection-question.component';
 
 // MATERIAL COMPONENTS
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 //Componente Header
@@ -35,6 +36,7 @@ import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProfileComponent } from './serpro-components/profile/profile.component';
 import { MedalComponent } from './serpro-components/profile/medal/medal.component'
+
 
 
 
@@ -68,7 +70,8 @@ import { MedalComponent } from './serpro-components/profile/medal/medal.componen
     MatIconModule,
     NgbModule
   ],
-  providers: [QuestionsService],
+  providers: [QuestionsService], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+//
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,34 +1,50 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LoginModel } from '../models/login.model';
 import { ScoreModel } from '../models/score.model';
 import { environment } from '../../environments/environment';
+import { RequestOptions, RequestOptionsArgs } from '@angular/http';
 import { Router } from '@angular/router';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,     private router: Router    ) {
 
   }
 
+
+
   postLogin(loginModule: LoginModel) {
+
+
     console.log("Metodo PostLogin", loginModule.email)
     console.log(loginModule.password)
 
-    return this.http.post(`${environment.urlQuestionApi}/login`, loginModule).subscribe(
-      (response) => {
-        console.log(response);
-        window.location.href = 'listaModulos';
-      },
-      error => {
-        console.log("entras?")
-        window.location.href = 'login';
+    return this.http.post(`${environment.urlQuestionApi}/login`, loginModule,{
+      withCredentials: true 
+    }).subscribe(
+       (response) => {
+          console.log(response);
+          this.router.navigate(['listaModulos'])
+          //location.assign( 'listaModulos');
+        }, 
+        error => {
+          console.log("Esta entrando en el error de login!")
+          //window.location.href = 'login';
 
-        console.log(error);
-      }
-    );
+          console.log("Error del front --> ",error);
+        }
+      );
   }
+
+
+
+
+
+
 }
