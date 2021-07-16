fc93e0785f4761bc7c79ec903a486d256a5063f2
volfram <ing.wsoto@gmail.com>
2021-07-16 17:47:05 -0500

    SessionCookie OK

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,35 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { LoginModel } from '../models/login.model';
 import { ScoreModel } from '../models/score.model';
 import { environment } from '../../environments/environment';
-import { RequestOptions, RequestOptionsArgs } from '@angular/http';
 import { Router } from '@angular/router';
-
 
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class LoginService {
 
-  constructor(private http: HttpClient,     private router: Router    ) {
+  constructor(private http: HttpClient, private router: Router,) {
 
   }
 
-
-
   postLogin(loginModule: LoginModel) {
 
-
     console.log("Metodo PostLogin", loginModule.email)
-    console.log(loginModule.password)
+    //console.log(loginModule.password)
 
     return this.http.post(`${environment.urlQuestionApi}/login`, loginModule,{
       withCredentials: true 
     }).subscribe(
        (response) => {
-          console.log(response);
+          console.log("Hi",response['userName'], "Welcome...");
           this.router.navigate(['listaModulos'])
           //location.assign( 'listaModulos');
         }, 
@@ -41,10 +37,5 @@ export class LoginService {
         }
       );
   }
-
-
-
-
-
 
 }
