fdbe61eb3184e7e7ff5a3e81f1ff35aefe22c646
volfram <ing.wsoto@gmail.com>
2021-07-17 12:55:43 -0500

    Logout

--- src/app/serpro-components/header/header.component.ts
+++ src/app/serpro-components/header/header.component.ts
@@ -1,19 +1,24 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Injectable, Input, OnInit} from '@angular/core';
 import { UserSettingsVariablesService } from 'src/app/services/user-settings-variables.service';
+import { LoginService } from 'src/app/services/login.service'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   @Input() title: string;
   pageTitle: string;
-  constructor(userSettingsVariablesService: UserSettingsVariablesService) { 
+  constructor(userSettingsVariablesService: UserSettingsVariablesService, private loginService: LoginService) { 
     this.pageTitle = userSettingsVariablesService.getPageTitle ();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
   }
 
+  methodLogout () {
+    this.loginService.postLogout();
+  }
 }
