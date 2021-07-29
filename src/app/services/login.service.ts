ef930f5b236a2b91dc6fe9f6d191090ef60681b4
Juan Pablo <jbernal@intelecto.co>
2021-07-28 19:44:17 -0500

    Solución, linea sin borrar en login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LoginModel } from '../models/login.model';
-import { ScoreModel } from '../models/score.model';
 import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
 
