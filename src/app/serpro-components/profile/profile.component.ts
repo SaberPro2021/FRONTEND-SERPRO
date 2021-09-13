67f5c5a37cf291d7273354cb73e29387c76e08a6
Juan Pablo <juanpis0304@hotmail.com>
2021-09-12 22:09:53 -0500

    Se agrega servicio de imagen del backend base de datos

--- src/app/serpro-components/profile/profile.component.ts
+++ src/app/serpro-components/profile/profile.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IcfesModule } from 'src/app/models/module.model';
-import { IcfesTest } from 'src/app/models/test.model';
 
+import { Observable } from 'rxjs';
+import { profile } from 'src/app/models/profile.model';
+import { userImageService } from '../../services/userImage.service';
 @Component({
   selector: 'serpro-profile',
   templateUrl: './profile.component.html',
@@ -11,16 +14,27 @@ export class ProfileComponent implements OnInit {
 
   userName : string;
   email : string;
+  image : string;
   module:IcfesModule;
   sizeTemporal: Number[];
 
-  constructor() { 
+
+  userId: string;
+  user: Observable<profile[]>;
+
+  constructor(private route: ActivatedRoute, private userImageService: userImageService) { 
     this.sizeTemporal = [1,2,3,4,5];
     this.userName = sessionStorage.getItem('userNameSession');
     this.email = sessionStorage.getItem('emailSession');
+    this.image = sessionStorage.getItem('imageSession');
+    
   }
 
   ngOnInit(): void {
+   
   }
 
+
+  
+ 
 }
